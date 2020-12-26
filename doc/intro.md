f450b10c0c6833ad5e18629625a474c8f0995abd
Gabriel <gabrieldavid_98@hotmail.com>
2020-12-26 14:03:42 -0500

    version 1

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to parenthmazes
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/parenthmazes/core_test.clj
@@ -0,0 +1,7 @@
+(ns parenthmazes.core-test
+  (:require [clojure.test :refer :all]
+            [parenthmazes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
