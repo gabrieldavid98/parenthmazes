89de896977da776460f212eb5f31cb0d211c3f59
Gabriel <gabrieldavid_98@hotmail.com>
2020-12-26 14:27:35 -0500

    version 2

--- src/parenthmazes/core.clj
+++ src/parenthmazes/core.clj
@@ -9,23 +9,32 @@
 (def weapon-damage 
   {:fists 10.0, :staff 35.0, :sword 100.0, :cast-iron-saucepan 150.0})
 
+(def weapon-fn-map
+  {:fists (fn [health] (if (< health 100) (- health 10) health))
+   :staff (partial + 35)
+   :sword #(- % 100)
+   :cast-iron-saucepan #(- % 100 (rand-int 50))
+   :sweet-potato identity})
+
 (defn strike
-  "With one argument, strike a target with a default :fists `weapon`. With two 
-   argument, strike a target with `weapon`.
-   Strike will heal a target that belongs to the gnomes camp."
+  "With one argument, strike a target with a default :fists `weapon`. With two
+   argument, strike a target with `weapon` and return the target entity"
   
   ([target] (strike target :fists))
   
-  ([{:keys [camp armor], :or {armor 0}, :as target} weapon]
-   (let [points (weapon weapon-damage)]
-     (if (= :gnomes camp)
-       (update target :health + points)
-       (let [damage (* points (- 1 armor))]
-         (update target :health - damage))))))
+  ([target weapon]
+   (let [weapon-fn (weapon weapon-fn-map)]
+     (update target :health weapon-fn))))
 
 (def enemy 
   {:name "Zulkaz", :health 250, :armor 0.8, :camp :trolls})
 
 (def ally {:name "Carla", :health 80, :camp :gnomes})
+
+(defn mighty-strike
+  "Strike a `target` with all weapons!"
+  [target]
+  (let [weapon-fn (apply comp (vals weapon-fn-map))]
+    (update target :health weapon-fn)))
 
 
